d3feb026647ccf8feeaf6b6e542b922a3a932701
Fox <foxtherigger@gmail.com>
2023-08-19 22:13:14 +0300

    Update deepwoken.sh

--- deepwoken.sh
+++ deepwoken.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
 # Bro, I should stop cooking demonic stuff bro this is wild!!!
-# ____        _          _____      _     _
+#____        _          _____      _     _
 #|  _ \ _   _| | _____  | ____|_ __(_)___(_) __ _
 #| | | | | | | |/ / _ \ |  _| | '__| / __| |/ _` |
 #| |_| | |_| |   <  __/ | |___| |  | \__ \ | (_| |
 #|____/ \__,_|_|\_\___| |_____|_|  |_|___/_|\__,_|
-
+clear
+echo " ____                               _" | lolcat
+sleep 1
+echo "|  _ \  ___  ___ _ ____      _____ | | _____ _ __ " | lolcat
+sleep 1
+echo "| | | |/ _ \/ _ \ '_ \ \ /\ / / _ \| |/ / _ \ '_ \ " | lolcat
+sleep 1
+echo "| |_| |  __/  __/ |_) \ V  V / (_) |   <  __/ | | | " | lolcat
+sleep 1
+echo "|____/ \___|\___| .__/ \_/\_/ \___/|_|\_\___|_| |_| " | lolcat
+sleep 1
+echo "                |_| " | lolcat
+sleep 1
+clear
 read -p "Enter your name Woken Soul... " woken_name
-
+clear
 woken_level=20
 woken_hp=140 # Usually a max lvl deepwoken build has around 450-470 hp
 woken_max_hp=140
 
 boss_name="Duke Erisia"
 boss_level=20
-boss_hp=136 # Should've been 15k but I gotta make this fair lmao
-boss_max_hp=136
+boss_hp=150 # Should've been 15k but I gotta make this fair lmao
+boss_max_hp=150
 
 echo "$boss_name, approaches you"
 sleep 2
@@ -49,7 +62,7 @@ echo "What are you going to do.. Woken Soul?"
 function battle() {
 
     clear
-    echo "$boss_name (Level $boss_level) blocks your way!"
+    echo "$boss_name (Level $boss_level) blocks your way"
     sleep 2 
     
     while [[ $woken_hp -gt 0 && $boss_hp -gt 0 ]]; do
@@ -57,8 +70,9 @@ function battle() {
         echo "$boss_name's HP: $boss_hp/$boss_max_hp"
 
         sleep 3
-        echo "1. Attack"
-        echo "2. Run" 
+        echo "1. Attack" | lolcat
+        echo "2. Defend" | lolcat
+        echo "3. Run"    | lolcat
         read -p "Choose an action: " choice
 
         case $choice in
@@ -71,7 +85,24 @@ function battle() {
                 echo "You casted 'Astral Wind', it dealt $woken_attack damage to $boss_name"
                 echo "$boss_name casted 'PILLARS OF ERISIA', it dealt $boss_attack damage to you"
                 ;;
-            2) 
+            2)
+                woken_defense=$((1 + RANDOM % 5))
+                boss_attack=$((1 + RANDOM % 3))
+                if [[ $boss_attack -gt $woken_defense ]]; then
+                    damage_taken=$((boss_attack - woken_defense))
+                    woken_hp=$((woken_hp - damage_taken))
+                    echo "$woken_name chose to defend. $woken_name blocked $woken_defense damage"
+                    if [[ $((RANDOM % 2)) -eq 0 ]]; then
+                        echo "$woken_name counterattacked, dealing $woken_defense damage to $boss_name"
+                        boss_hp=$((boss_hp - woken_defense))
+                    fi
+                    echo "$boss_name's attack dealt $damage_taken damage to you"
+                else
+                    echo "$woken_name chose to defend, but the defense was strong enough to completely block $boss_name's attack"
+                fi
+                ;;
+            
+            3) 
                 echo "You ran away"
                 sleep 2
                 echo "$boss_name: I knew that you were a coward ever since you entered this manor, I have a feeling that we'll meet again.."
